e2e2ae7ea19c0b25ef9ae94542f33de9517ef00d
Colin Keil <cwk.321@gmail.com>
2025-07-08 16:51:39 -0400

    Update to use new symbolifally generated solutions

--- brute_force_link_loads_ananlysis.py
+++ brute_force_link_loads_ananlysis.py
@@ -2,28 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from uarm_kinematics_loads_simulation import linkage_robot
 
-robot = linkage_robot()
-
 simple = False
+robot = linkage_robot(simple)
+
 # pos = [np.pi/4, - np.pi/4]
 # robot.calculate_kinematics(pos)
 
-x_grid_size = 4
-y_grid_size = 5
+y_grid_size = 11
+z_grid_size = 21
 # positions = np.linspace(0.1,1,grid_size)
-xpos = np.linspace(0.1,1,x_grid_size)
 ypos = np.linspace(0.1,1,y_grid_size)
-xx,yy = np.meshgrid(xpos, ypos)
+zpos = np.linspace(-1,1,z_grid_size)
+yy,zz = np.meshgrid(ypos, zpos)
 ee_loads = (np.array([0,1]),np.array([1,0]),np.array([1,1]),np.array([-1,1]))
 max_torque = 32
-max_loads = [np.zeros((y_grid_size,x_grid_size)) for _ in ee_loads]
+max_loads = [np.zeros((z_grid_size,y_grid_size)) for _ in ee_loads]
 max_torques = []
 
 
-for j, x in enumerate(xpos):
-    for i, y in enumerate(ypos):
+for j, y in enumerate(ypos):
+    for i, z in enumerate(zpos):
         #note i,j vs x,y complies with matplotlib convention
-        pos = [x,y]
+        pos = [y,z]
         
         joint_pos = robot.inverse_kinematics(pos)
         if np.any(np.isnan(joint_pos)):
@@ -31,10 +31,11 @@ for j, x in enumerate(xpos):
                 ml[i,j] = np.nan
             continue
         
-        robot.calculate_kinematics(joint_pos)
+        # robot.calculate_kinematics(joint_pos)
 
         for max_load, ee_load in zip(max_loads, ee_loads):
-            results = robot.calculate_static_loads( ee_load = ee_load,simple = simple)
+            results = robot.compute_static_loads_symbolic(joint_pos, ee_load = ee_load)
+            # results = robot.calculate_static_loads( ee_load = ee_load,simple = simple)
             T1, T2 = np.abs(results["T1"]), np.abs(results["T2"])
             T = T1 if T1 > T2 else T2
             t_factor = (max_torque/T)
@@ -45,15 +46,15 @@ for j, x in enumerate(xpos):
 fig, axes = plt.subplots(2,2)
 ims = []
 for ax, loads, vect in zip(axes.reshape(-1),max_loads,ee_loads):
-    print(xx.shape)
-    print(yy.shape)
-    print(loads.shape)
-    pm= ax.pcolormesh(xx, yy, loads, shading='nearest')
+    # print(yy.shape)
+    # print(zz.shape)
+    # print(loads.shape)
+    pm= ax.pcolormesh(yy, zz, loads, shading='nearest')
     ims.append(pm)
     plt.colorbar(pm, ax = ax, label='Nm')
     ax.set_title(f"force vector {vect}")
-    ax.set_xlabel('x')
     ax.set_xlabel('y')
+    ax.set_ylabel('z')
 # fig.colorbar(ims[0], ax=axes[0])
 plt.show()
 # fig, ax = plt.subplots()
